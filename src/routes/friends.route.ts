90eb55aaed3ec99d40b573fead15b0f17b83a82a
Hyun A Kim <hyuna@Violet.local>
2024-01-25 02:48:19 +0900

    merge fix

--- src/routes/friends.route.ts
+++ src/routes/friends.route.ts
@@ -14,4 +14,4 @@ router.get("/get-friends/:userId", auth, FriendsController.getFriends);
 // 친구의 맵 보기 라우트
 // router.get("/view-friend-map/:friendId", FriendsController.viewFriendMap);
 
-export default router;
+module.exports = router;
